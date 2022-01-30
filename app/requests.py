fae0919e61cd1d45b618e7ff43cebdf3c3e7b2d7
erhic <eric.gichovi@student.moringaschool.com>
2022-01-30 19:02:38 +0300

    added a  function to get the api key and url from the config

--- app/requests.py
+++ app/requests.py
@@ -1,2 +1,17 @@
 import urllib.request,json
 from .models import News
+
+#sources
+
+# Getting api key
+Api_key = None
+# Getting source url
+Base_url= None
+
+def configure_request(app):
+    global Api_key, Base_url
+    Api_key = app.config['NEWS_API_KEY']
+    Base_url= app.config['NEWS_API_BASE_URL']
+    
+
+    
