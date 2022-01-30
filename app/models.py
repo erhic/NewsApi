c45bd691786a46801ee07c3d847e02972062dfcf
erhic <eric.gichovi@student.moringaschool.com>
2022-01-30 18:43:39 +0300

    added a class to create object

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    class to define news  object
+    '''
+    def __init__(self ,title, description, urlToImage,content,publishedAt):
+        self.title=title
+        self.description= description
+        self.urlToImage= urlToImage
+        self.content=content
+        self.publishedAt=publishedAt
+                    
