27146c36609cfc9329e40df718ad6d7e4833326f
erhic <eric.gichovi@student.moringaschool.com>
2022-01-30 17:27:48 +0300

    registering the blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -3,4 +3,17 @@ from flask import Flask
 # Initializing application
 app = Flask(__name__)
 
+def create_app(config_name):
+    #....
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
+
+
 from app import views
