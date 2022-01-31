66edb599461d8a7693b69d5232456ec4bff7455e
erhic <eric.gichovi@student.moringaschool.com>
2022-01-31 21:23:48 +0300

    set url variable for the constructor

--- app/models.py
+++ app/models.py
@@ -2,10 +2,11 @@ class News:
     '''
     class to define news  object
     '''
-    def __init__(self ,title, description, urlToImage,content,publishedAt):
+    def __init__(self ,title, description, urlToImage,url,content,publishedAt):
         self.title=title
         self.description= description
         self.urlToImage= urlToImage
+        self.url=url
         self.content=content
         self.publishedAt=publishedAt
                     
